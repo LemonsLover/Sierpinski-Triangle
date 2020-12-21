02dadebc4bdeb002c391b5d72cf9321d3821c706
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2020-12-21 20:17:19 +0200

    Sierpinski Triangle by Yehor Ovseukov

---
+++ Sierpinski_Triangle/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Sierpinski_Triangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.numericUpDownDotWidth = new System.Windows.Forms.NumericUpDown();
+            this.labelDotWidth = new System.Windows.Forms.Label();
+            this.timerDraw = new System.Windows.Forms.Timer(this.components);
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.comboBoxColor = new System.Windows.Forms.ComboBox();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.pictureBoxLine = new System.Windows.Forms.PictureBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.numericUpDownDotSpeed = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotWidth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreen
+            // 
+            this.pictureBoxScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxScreen.Location = new System.Drawing.Point(3, 2);
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.Size = new System.Drawing.Size(500, 500);
+            this.pictureBoxScreen.TabIndex = 0;
+            this.pictureBoxScreen.TabStop = false;
+            this.pictureBoxScreen.Resize += new System.EventHandler(this.pictureBoxScreen_Resize);
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDraw.Location = new System.Drawing.Point(513, 12);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(139, 35);
+            this.buttonDraw.TabIndex = 1;
+            this.buttonDraw.Text = "Нарисовать";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // numericUpDownDotWidth
+            // 
+            this.numericUpDownDotWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownDotWidth.DecimalPlaces = 1;
+            this.numericUpDownDotWidth.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDownDotWidth.Location = new System.Drawing.Point(513, 119);
+            this.numericUpDownDotWidth.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownDotWidth.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            65536});
+            this.numericUpDownDotWidth.Name = "numericUpDownDotWidth";
+            this.numericUpDownDotWidth.Size = new System.Drawing.Size(139, 20);
+            this.numericUpDownDotWidth.TabIndex = 2;
+            this.numericUpDownDotWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotWidth.ValueChanged += new System.EventHandler(this.numericUpDownDotWidth_ValueChanged);
+            // 
+            // labelDotWidth
+            // 
+            this.labelDotWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDotWidth.AutoSize = true;
+            this.labelDotWidth.Location = new System.Drawing.Point(510, 100);
+            this.labelDotWidth.Name = "labelDotWidth";
+            this.labelDotWidth.Size = new System.Drawing.Size(80, 13);
+            this.labelDotWidth.TabIndex = 3;
+            this.labelDotWidth.Text = "Размер точек:";
+            // 
+            // timerDraw
+            // 
+            this.timerDraw.Interval = 1000;
+            this.timerDraw.Tick += new System.EventHandler(this.timerDraw_Tick);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(658, 12);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(139, 35);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "СТОП";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonClean.Location = new System.Drawing.Point(591, 53);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(139, 35);
+            this.buttonClean.TabIndex = 5;
+            this.buttonClean.Text = "ОЧИСТИТЬ";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // comboBoxColor
+            // 
+            this.comboBoxColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxColor.FormattingEnabled = true;
+            this.comboBoxColor.Items.AddRange(new object[] {
+            "Red",
+            "Blue",
+            "Green",
+            "Black",
+            "Gray"});
+            this.comboBoxColor.Location = new System.Drawing.Point(658, 119);
+            this.comboBoxColor.Name = "comboBoxColor";
+            this.comboBoxColor.Size = new System.Drawing.Size(139, 21);
+            this.comboBoxColor.TabIndex = 6;
+            // 
+            // labelColor
+            // 
+            this.labelColor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelColor.AutoSize = true;
+            this.labelColor.Location = new System.Drawing.Point(655, 100);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(66, 13);
+            this.labelColor.TabIndex = 7;
+            this.labelColor.Text = "Цвет точек:";
+            // 
+            // pictureBoxLine
+            // 
+            this.pictureBoxLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxLine.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBoxLine.Location = new System.Drawing.Point(500, -26);
+            this.pictureBoxLine.Name = "pictureBoxLine";
+            this.pictureBoxLine.Size = new System.Drawing.Size(10, 589);
+            this.pictureBoxLine.TabIndex = 8;
+            this.pictureBoxLine.TabStop = false;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(755, 481);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(33, 13);
+            this.labelTitle.TabIndex = 9;
+            this.labelTitle.Text = "By: Я";
+            // 
+            // numericUpDownDotSpeed
+            // 
+            this.numericUpDownDotSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDownDotSpeed.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Location = new System.Drawing.Point(591, 159);
+            this.numericUpDownDotSpeed.Maximum = new decimal(new int[] {
+            1337,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.Name = "numericUpDownDotSpeed";
+            this.numericUpDownDotSpeed.Size = new System.Drawing.Size(139, 20);
+            this.numericUpDownDotSpeed.TabIndex = 10;
+            this.numericUpDownDotSpeed.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDotSpeed.ValueChanged += new System.EventHandler(this.numericUpDownDotSpeed_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Скорось точек:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownDotSpeed);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.pictureBoxLine);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.comboBoxColor);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelDotWidth);
+            this.Controls.Add(this.numericUpDownDotWidth);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.pictureBoxScreen);
+            this.Name = "Form1";
+            this.Text = "Треугольник Овсеюкова";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotWidth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDotSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.NumericUpDown numericUpDownDotWidth;
+        private System.Windows.Forms.Label labelDotWidth;
+        private System.Windows.Forms.Timer Draw_Tick;
+        private System.Windows.Forms.Timer timerDraw;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonClean;
+        private System.Windows.Forms.ComboBox comboBoxColor;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.PictureBox pictureBoxLine;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.NumericUpDown numericUpDownDotSpeed;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Sierpinski_Triangle/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sierpinski_Triangle
+{
+    public partial class Form1 : Form
+    {
+        public float dotWidth;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        List<PointF> Corners;
+
+        PointF LastPoint;
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            timerDraw.Enabled = true;
+            dotWidth = Convert.ToInt64(numericUpDownDotWidth.Value);
+        }
+
+        private void DefineCorners()
+        {
+            Corners = new List<PointF>();
+            Corners.Add(new PointF(pictureBoxScreen.Width / 2, 10));
+            Corners.Add(new PointF(10, pictureBoxScreen.Height - 10));
+            Corners.Add(new PointF(pictureBoxScreen.Width - 10, pictureBoxScreen.ClientSize.Height - 10));
+
+
+            LastPoint = Corners[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DefineCorners();
+            numericUpDownDotSpeed_ValueChanged(sender, e);
+            comboBoxColor.SelectedIndex = 0;
+        }
+
+        private void timerDraw_Tick(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+
+                for (int i = 1; i <= 1000; i++)
+                {
+                    int j = rand.Next(0, 3);
+                    LastPoint = new PointF(
+                        (LastPoint.X + Corners[j].X) / 2,
+                        (LastPoint.Y + Corners[j].Y) / 2);
+                    Pen pen = new Pen(Color.FromName(comboBoxColor.SelectedItem.ToString()), dotWidth);
+                    gr.DrawEllipse(pen, LastPoint.X, LastPoint.Y,
+                        dotWidth, dotWidth);
+                }
+            }
+        }
+
+        private void pictureBoxScreen_Resize(object sender, EventArgs e)
+        {
+            timerDraw.Enabled = false;
+            DefineCorners();
+
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+                gr.Clear(this.BackColor);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timerDraw.Enabled = false;
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e)
+        {
+            using (Graphics gr = pictureBoxScreen.CreateGraphics())
+            {
+                gr.Clear(this.BackColor);
+            }
+            timerDraw.Enabled = false;
+        }
+
+        private void numericUpDownDotSpeed_ValueChanged(object sender, EventArgs e)
+        {
+            timerDraw.Interval = Convert.ToInt16(numericUpDownDotSpeed.Value);
+        }
+
+        private void numericUpDownDotWidth_ValueChanged(object sender, EventArgs e)
+        {
+            dotWidth = Convert.ToInt64(numericUpDownDotWidth.Value);
+        }
+    }
+}
